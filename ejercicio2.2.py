39cf8d1df5b3779ec333116dc7b3d29ea0acd716
TheRealDiego333 <99895286+TheRealDiego333@users.noreply.github.com>
2022-02-24 22:28:13 -0600

    Add files via upload

--- ejercicio2.2.py
+++ ejercicio2.2.py
@@ -17,18 +17,21 @@ while (True):
 		a2=float(input("Ingrese el segundo angulo: "))
 	except ValueError:
 		print("Entrada invalida")
-	try:
-		a3=180-a1-a2
-		print("el tercer angulo es: ",a3)
-		cursor = conexion.cursor()
-		cursor.execute (INSERT,(a1,a2,a3))
-		conexion.commit()
-	except NameError:
-		print("Ingrese un numero")	
-	m = input("Quieres ver el historial de datos?(y/n): ")
-	if m == 'y' or m == 'Y':
-		cursor = conexion.cursor()
-		cursor.execute(SQL)
-		all_values = cursor.fetchall()
-		print('Historial: ',  all_values)
+	if 0< a1 < 180 and 0<a2 <180:
+		try:
+			a3=180-a1-a2
+			print("el tercer angulo es: ",a3)
+			cursor = conexion.cursor()
+			cursor.execute (INSERT,(a1,a2,a3))
+			conexion.commit()
+		except NameError:
+			print("Ingrese un numero")	
+		m = input("Quieres ver el historial de datos?(y/n): ")
+		if m == 'y' or m == 'Y':
+			cursor = conexion.cursor()
+			cursor.execute(SQL)
+			all_values = cursor.fetchall()
+			print('Historial: ',  all_values)
+	else:
+		print("Angulos invalidos")
 
